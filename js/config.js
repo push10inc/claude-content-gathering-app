825f9e2fc4857ff5f5ee23cb7c729be94971c051
push10inc <webmaster@push10.com>
2026-09-18 11:52:29 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // Paste your deployed Apps Script Web App URL here (ends in /exec).
 // See README.md > "Deploy the backend" for how to get this.
 window.CGT_CONFIG = {
-  API_URL: 'https://script.google.com/a/macros/push10.com/s/AKfycbwF3z0GfhjJAKH65wS5IkkXQWnrr6o4eCHg73fii3BOFKNew8YuchAZQzdrKIVuGkF1/exec'
+  API_URL: 'https://script.google.com/macros/s/AKfycbx5Maa8NBcpGU-ItL_KTAe_NH40QD2CNXcZwSQP___Y7Dp4VxerBgm1R0jKFQOU7ZKI/exec'
 };
